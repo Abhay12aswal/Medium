d08b3a4d347702bc537951da1618af32d199e8e6
Abhay12Aswal <abhayaswal939@gmail.com>
2024-06-06 21:44:46 +0530

    fe

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,13 +1,29 @@
 import { signupInput } from '@abhay_aswal/medium-common';
 import { useState , ChangeEvent } from 'react';
-import {Link, json} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom';
+import axios from "axios";
+import { BACKEND_URL } from '../config';
 
 const Auth = ({type}: {type: "signup" | "signin"}) => {
+  const Navigate = useNavigate();
   const [postInputs , setpostInputs] = useState<signupInput>({
     name:"",
     username:"",
     password:""
-  })
+  });
+
+  async function sendRequest (){
+    try{
+      const response =await axios.post(`${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}` , postInputs);
+      const jwt =  response.data;
+      localStorage.setItem("token", jwt);
+      Navigate("/blogs");
+    } catch(e){
+      //alert the user here that the request failed 
+      alert("request failed")
+
+    }
+  }
 
   return (
     <div className='h-screen flex justify-center flex-col'>
@@ -18,17 +34,20 @@ const Auth = ({type}: {type: "signup" | "signin"}) => {
                         Create an account
                 </div>
                 <div className="text-slate-400 ">
-                    Already have an account? 
-                    <Link className='pt-2 underline' to={"/signin"}>Login</Link>
+                    {type === "signin" ? "Don't have an account" : "Already have an account ? " }
+                    <Link className='pt-2 underline' to={type === "signin" ? "/signup" : "/signin"}>
+                    {type === "signin" ? "sign up " : "sign in "}
+                    </Link>
                 </div>
               </div>
-              <div className='pt-4'>
-                <LabelledInput label="Name" placeholder='user ' onChange={(e)=>{
+              <div className='pt-8'>
+                {type === "signup" ? <LabelledInput label="Name" placeholder='user ' onChange={(e)=>{
                   setpostInputs(c => ({
                     ...postInputs,
                     name: e.target.value
-                  }))
-                }} />
+                    }))
+                  }} /> : null
+                }
 
                 <LabelledInput label="Username" placeholder='user@gmail.com' onChange={(e)=>{
                   setpostInputs(c => ({
@@ -46,7 +65,7 @@ const Auth = ({type}: {type: "signup" | "signin"}) => {
                 <button type="button" className="text-white w-full bg-gray-800 hover:bg-gray-900 
                 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium 
                  rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800  dark:hover:bg-gray-700
-                dark:focus:ring-gray-700 dark:border-gray-700">{type ===""}</button>
+                dark:focus:ring-gray-700 dark:border-gray-700">{type ==="signup" ? "sign up" : "sign in"}</button>
               </div>
             </div>
        </div>
--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,10 +1,20 @@
+import Auth from "../components/Auth"
+import Quote from "../components/Quote"
 
-const Signin = () => {
+
+const signin = () => {
   return (
     <div>
-      <p>signin</p>
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+            <div>
+                <Auth type="signin"/>
+            </div>
+            <div className="hidden lg:block">
+                <Quote/>
+            </div>
+        </div>
     </div>
   )
 }
 
-export default Signin
+export default signin
